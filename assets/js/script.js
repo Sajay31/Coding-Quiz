be344bd6bb38dd2cf0fe33aa003da7ef60fc1c18
Sajay31 <dentheo769@gmail.com>
2023-02-12 14:34:33 -0500

    completed comments for code

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,8 +40,9 @@ const handleAns = function(choice) {
 }
 
 const handleQA = () => {
-
+// handles all question responses and when done, end function.
     if(qI==questions.length) return done();
+
 
     let {Q,A} = questions[qI];
     main.innerHTML = `<h1>${Q}</h1><div id='answers'></div>`;
@@ -50,22 +51,24 @@ const handleQA = () => {
         document.getElementById('answers').innerHTML+= `<button onclick="handleAns('${ans}')">${ans}</button>`;
     });
 };
-
+//This takes submitted inatials and scores then stores it to local storage. 
 const subFx = () => {
     let initals = document.getElementById('initials').value;
     store.push({initials:initals,score:Math.floor(time*60/100)});
     localStorage.highScore = JSON.stringify(store);
+    // this logs all the scores and restarts quiz when user clicks on the go back button.
     main.innerHTML=`<h1>Highscores</h1>
     <ul></ul>
     <button onclick="location.reload()">Go Back</button>`;
 
+    //puts user score and initials in console log
     store.forEach(player => {
         console.log(player);
         document.querySelector('ul').innerHTML+=`<li>${player.initials}: ${player.score}</li>`
     });
 }
 
-
+// page for when quiz is completed.
 const done = () => {
     main.innerHTML=`<h1>All Done!!!</h1>
     <h4>Your final score: ${Math.floor(time*60/100)} </h4>
@@ -76,7 +79,7 @@ const done = () => {
 const highScore = () => {
    
 }
-
+// restarts timer and runs questions
 start.onclick = () => {
 
     clearInterval(clockId);
